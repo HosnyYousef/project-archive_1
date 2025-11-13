29a51805b64fa44f7672d19dfd0d90cc6c6cae6e
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-11-13 15:36:04 +0200

    Setting up navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NavigationBar />
           {children}
         </body>
       </html>
