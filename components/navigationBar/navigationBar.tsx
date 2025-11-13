2ccfd1aa36bd4588ad9e5f649f3d7ec95d816ec7
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-11-13 14:41:09 +0200

    first commit

---
+++ components/navigationBar/navigationBar.tsx
@@ -0,0 +1,8 @@
+export default function NavigationBar() {
+    return (
+        <header>
+            <nav className='w-full flex text-black items-center justify-between bg-white p-2 border-b border-slate-300'>
+            </nav>
+        </header>
+    )
+}
